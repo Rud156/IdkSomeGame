ec74e741cf82f9138a95965d13647a95ab88f4dc
Rud156 <rudrangshu_1997@yahoo.com>
2026-08-16 19:02:39 -0400

    Adding Player Control

---
+++ Assets/Scripts/Player/PlayerAnimationController.cs
@@ -0,0 +1,78 @@
+using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerAnimationController : MonoBehaviour
+    {
+        private static readonly int Jump = Animator.StringToHash("Jump");
+        private static readonly int Moving = Animator.StringToHash("Running");
+        private static readonly int Falling = Animator.StringToHash("Falling");
+
+
+        [Header("Components")]
+        [SerializeField] private Animator _animator;
+        [SerializeField] private PlayerController _playerController;
+
+        private void Start()
+        {
+            _playerController.onJumped += HandlePlayerJumped;
+            _playerController.onGroundedStateChanged += HandleGroundedStateChanged;
+            _playerController.onStateChanged += HandlePlayerStateChanged;
+        }
+
+        private void OnDestroy()
+        {
+            _playerController.onJumped -= HandlePlayerJumped;
+            _playerController.onGroundedStateChanged -= HandleGroundedStateChanged;
+            _playerController.onStateChanged -= HandlePlayerStateChanged;
+        }
+
+        #region State Functions
+
+        private void HandlePlayerJumped()
+        {
+            _animator.SetTrigger(Jump);
+        }
+        
+        private void HandleGroundedStateChanged(bool currentState)
+        {
+            _animator.SetBool(Falling, !currentState);
+        }
+
+        private void HandlePlayerStateChanged(PlayerState currentState, PlayerState previousState)
+        {
+            switch (currentState)
+            {
+                case PlayerState.Idle:
+                    _animator.SetBool(Moving, false);
+                    break;
+
+                case PlayerState.Moving:
+                    _animator.SetBool(Moving, true);
+                    break;
+
+                case PlayerState.Falling:
+                    _animator.SetBool(Falling, true);
+                    break;
+
+                case PlayerState.Slide:
+                    break;
+                case PlayerState.WallRun:
+                    break;
+                case PlayerState.RailGrind:
+                    break;
+                case PlayerState.Ability1:
+                    break;
+                case PlayerState.Ability2:
+                    break;
+                case PlayerState.Ability3:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(currentState), currentState, null);
+            }
+        }
+
+        #endregion
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -10,6 +10,12 @@ namespace Player
         [Header("Components")]
         [SerializeField] private CharacterController _characterController;
 
+        [Header("Movement")]
+        [SerializeField] private float _moveSpeed;
+        [SerializeField] private float _accelerationRate;
+        [SerializeField] private float _decelerationRate;
+        [SerializeField] private float _jumpLaunchSpeed;
+
         // Inputs
         private InputAction _moveAction;
         private InputAction _lookAction;
@@ -18,6 +24,17 @@ namespace Player
 
         // Player State
         private Stack<PlayerState> _playerStateStack;
+        private bool _isGrounded;
+        private Vector3 _moveVelocity;
+
+        // Delegates
+        public delegate void OnJumped();
+        public delegate void OnGroundedStateChanged(bool currentState);
+        public delegate void OnStateChanged(PlayerState currentState, PlayerState previousState);
+
+        public OnJumped onJumped;
+        public OnGroundedStateChanged onGroundedStateChanged;
+        public OnStateChanged onStateChanged;
 
         private void Start()
         {
@@ -26,13 +43,24 @@ namespace Player
             _jumpAction = InputSystem.actions.FindAction("Jump");
             _sprintAction = InputSystem.actions.FindAction("Sprint");
 
-            _playerStateStack.Push(PlayerState.Idle);
+            _moveVelocity = Vector3.zero;
+
+            _playerStateStack = new Stack<PlayerState>();
+            PushState(PlayerState.Idle);
         }
 
         private void Update()
         {
+            UpdatePlayerMovement();
+        }
+
+        #region Player Movement
+
+        private void UpdatePlayerMovement()
+        {
             CheckForJump();
-            
+            CheckGroundedState();
+
             switch (_playerStateStack.Peek())
             {
                 case PlayerState.Idle:
@@ -65,13 +93,39 @@ namespace Player
                 default:
                     throw new ArgumentOutOfRangeException();
             }
-        }
 
-        #region Player State Handling
+            ApplyPlayerMovement();
+        }
 
         private void CheckForJump()
         {
+            if (!_characterController.isGrounded)
+            {
+                return;
+            }
+
+            _moveVelocity.y = _jumpLaunchSpeed;
         }
+
+        private void CheckGroundedState()
+        {
+            if (_isGrounded == _characterController.isGrounded)
+            {
+                return;
+            }
+
+            onGroundedStateChanged?.Invoke(_characterController.isGrounded);
+            _isGrounded = _characterController.isGrounded;
+        }
+
+        private void ApplyPlayerMovement()
+        {
+            _characterController.Move(_moveVelocity * Time.deltaTime);
+        }
+
+        #endregion
+
+        #region Player State Handling
 
         private void UpdateIdleState()
         {
@@ -107,6 +161,22 @@ namespace Player
 
         private void UpdateAbility3State()
         {
+        }
+
+        #endregion
+
+        #region State Functions
+
+        public void PushState(PlayerState playerState)
+        {
+            onStateChanged?.Invoke(playerState, _playerStateStack.Peek());
+            _playerStateStack.Push(playerState);
+        }
+
+        public void PopState()
+        {
+            var playerState = _playerStateStack.Pop();
+            onStateChanged?.Invoke(_playerStateStack.Peek(), playerState);
         }
 
         #endregion
