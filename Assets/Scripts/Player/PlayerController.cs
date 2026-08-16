1feb01a88de3f7111dc58dc568969be686b2dff2
Rud156 <rudrangshu_1997@yahoo.com>
2026-08-16 16:11:31 -0400

    Added Basic Player and Animations and Textures

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,5 +1,114 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+namespace Player
 {
+    public class PlayerController : MonoBehaviour
+    {
+        [Header("Components")]
+        [SerializeField] private CharacterController _characterController;
+
+        // Inputs
+        private InputAction _moveAction;
+        private InputAction _lookAction;
+        private InputAction _jumpAction;
+        private InputAction _sprintAction;
+
+        // Player State
+        private Stack<PlayerState> _playerStateStack;
+
+        private void Start()
+        {
+            _moveAction = InputSystem.actions.FindAction("Move");
+            _lookAction = InputSystem.actions.FindAction("Look");
+            _jumpAction = InputSystem.actions.FindAction("Jump");
+            _sprintAction = InputSystem.actions.FindAction("Sprint");
+
+            _playerStateStack.Push(PlayerState.Idle);
+        }
+
+        private void Update()
+        {
+            CheckForJump();
+            
+            switch (_playerStateStack.Peek())
+            {
+                case PlayerState.Idle:
+                    UpdateIdleState();
+                    break;
+                case PlayerState.Moving:
+                    UpdateMovingState();
+                    break;
+                case PlayerState.Falling:
+                    UpdateFallingState();
+                    break;
+                case PlayerState.Slide:
+                    UpdateSlideState();
+                    break;
+                case PlayerState.WallRun:
+                    UpdateWallRunState();
+                    break;
+                case PlayerState.RailGrind:
+                    UpdateRailGrindState();
+                    break;
+                case PlayerState.Ability1:
+                    UpdateAbility1State();
+                    break;
+                case PlayerState.Ability2:
+                    UpdateAbility2State();
+                    break;
+                case PlayerState.Ability3:
+                    UpdateAbility3State();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        #region Player State Handling
+
+        private void CheckForJump()
+        {
+        }
+
+        private void UpdateIdleState()
+        {
+        }
+
+        private void UpdateMovingState()
+        {
+        }
+
+        private void UpdateFallingState()
+        {
+        }
+
+        private void UpdateSlideState()
+        {
+        }
+
+        private void UpdateWallRunState()
+        {
+        }
+
+        private void UpdateRailGrindState()
+        {
+        }
+
+        private void UpdateAbility1State()
+        {
+        }
+
+        private void UpdateAbility2State()
+        {
+        }
+
+        private void UpdateAbility3State()
+        {
+        }
+
+        #endregion
+    }
 }
---
+++ Assets/Scripts/Player/PlayerState.cs
@@ -0,0 +1,20 @@
+﻿namespace Player
+{
+    public enum PlayerState
+    {
+        // Basic Movement
+        Idle,
+        Moving,
+        Falling,
+        
+        // Sprint Actions
+        Slide,
+        WallRun,
+        RailGrind,
+        
+        // Abilities
+        Ability1,
+        Ability2,
+        Ability3
+    }
+}
